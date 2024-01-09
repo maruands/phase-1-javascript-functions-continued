df18545f04bd21296b4d97b2b553ea4e05834573
maruands <maruands@gmail.com>
2024-01-09 21:05:48 +0300

    function with parameter

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(name = "roller-skate")
+{
+    return `This Saturday, I want to ${name}!`;
+}
+
+function mondayWork(office = "go to the office")
+{
+    return `This Monday, I will ${office}.`
+}
+
+function wrapAdjective(character)
+{
+    return function(adjective)
+    {
+        return `You are ${character}${adjective}${character}!`
+    }
+}
